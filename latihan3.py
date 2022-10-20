cf88ebdde2fe6f0be073dffc34d1e9b89fe411c1
Kudav5 <kdvalerian@gmail.com>
2022-10-21 06:42:38 +0700

    scriptnya

---
+++ latihan3.py
@@ -0,0 +1,16 @@
+# input nilai variabel
+a = input("masukkan nilai a: ")
+b = input("masukkan nilai b: ")
+
+# cetak nilai variabel
+print("Variabel a = ", a)
+print("Variabel b = ", b)
+
+# cetak hasil operasi kedua variabel dengan String Format
+print("Hasil penggabungan", a, '&', b, '=', "{1}{0}".format(a,b))
+
+# konversi nilai variabel
+a = int(a)
+b = int(b)
+print("hasil penjumlahan", a,'+',b,"= %d" %(a+b))
+print("hasil pembagian", a,'/',b,"= %d" %(a/b))
